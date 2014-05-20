cf999dd1556608859c783ea12a3228d1d86019ff
pacool <pacool@debian>
2014-05-20 22:41:17 +0200

    Début fonctions users : scanf et printf X

---
+++ minikernel-2013-14/minikernel_init/usr_functions.h
@@ -0,0 +1,7 @@
+/* Fichier usr_functions.h*/
+
+static void printf_str(const char* str, int len, char fillwith, int placeleft, int signe);
+void printf ( const char* fmt,...);
+int scanf_usr( const char* fmt, char* addr);
+
+
