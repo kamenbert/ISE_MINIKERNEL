c0ffc994d002e0b0ff2d07d872faee83c91af864
omegavschaos <omegavschaos@gmail.com>
2014-05-26 01:00:29 +0200

    Ajout de la library non compilé

---
+++ minikernel-2013-14/minikernel_user/libK.h
@@ -0,0 +1,7 @@
+/* Fichier usr_functions.h*/
+
+extern char read ();
+extern void write (char);
+extern void sleep (int);
+//extern void printf ( const char* fmt,...);
+//int scanf_usr( const char* fmt, char* addr);
--- minikernel-2013-14/minikernel_user/usr_functions.h
+++
@@ -1,7 +0,0 @@
-/* Fichier usr_functions.h*/
-
-char read ();
-void write (char);
-void sleep (int);
-void printf ( const char* fmt,...);
-//int scanf_usr( const char* fmt, char* addr);
