257c09d20a32609092315840a5b9bfcfe33d6269
pacool <pacool@debian>
2014-05-27 18:09:18 +0200

    Fonction Scanf

--- minikernel-2013-14/minikernel_init/usr_functions.c
+++ minikernel-2013-14/minikernel_init/usr_functions.c
@@ -112,64 +112,108 @@ va_end(args);
 }
 
 
-
-int scanf_usr( const char* fmt, char* addr){
-
-char*	buf=addr;// = malloc(BUF_LEN*sizeof(char));
+/* Sans sécurité, et débordement */
+int scanf( const char* fmt,...){
+va_list args;  
+va_start(args, fmt);
+char*	buf;// =NULL; 
 int 	i=0;
 char	c;
 int 	signe=0;
-while (*fmt) {
+int	nb=0;
+while (*fmt!='\0') {//printf("WHILE 1 : (%c)\n", *fmt);
 switch (*fmt) {
 case '%' :	
- 	fmt++;		
-   	switch(*fmt){     
-		case 'c' : 
-			c=read(); //read(1,&c,1);
-              	        if ( c> 32 && c<127) {
-				*buf = c;
-				i++;// Si un seul format dans scanf
+ 	fmt++;	
+	while (*fmt) {	//printf("WHILE2 : (%c)\n", *fmt);
+   	switch(*fmt){   
+		case 'c':
+			buf =  va_arg(args, char*);			
+			c=' ';
+			while(c=read()){
+				if(c>32 && c<255) break;
 			}
-			goto fin;
-		case 'd':	
-			//while(read(1,&c,1)){
-			if (c = read()){			
-				if (signe ==0 &&( c =='+'||c== '-') ){
-					signe = 1;
-					*(buf+i) = c;
-					i++;
-	 				}
-				if (c>'0' && c<='9'){	
-					*(buf+i) = c; /*printf("Nb : %d\n", (int) c); //COONVERTIR EN INT : ATOI stdlib, ou garder en char ? */
+              	        if (c> 32 && c<127) {
+				*(buf) =c;//printf("{ ___ (%c/%c)___ } \n",*buf, *(buf+1));
+				i++;
+			}
+			fmt++; 
+			goto next;
+
+		case 'd': //printf("START D\n\n");
+
+			//void* memcpy(void* dest, const void* src, size_t n)
+			int* nAcc= va_arg(args, int*);
+			int id=i; //Pour une entrée à plusieurs format, on renvoie le nombre de caractères déjà écrit avant l'entrée pour ce format s'il y a un dépassement INT_MAX
+			int max=1000000000;
+			
+			//int max= INT_MAX;
+			while(c=read()){
+				if(c== 32) continue;
+				else if (signe ==0 &&( c == 43||c== 45) ){ //printf("Vérif SIGNE\n");
+						if(c==45) signe = -1;
+						//i++;
+	 			}
+				else if(c>=48 && c<=59) break;
+			}
+			
+			do{ 					
+				if (c>=48 && c<=59){	
+					if(nb *10 +c-48 < max) {
+						nb = nb*10 + c-48;
+					}
+					else{ 	//nb=INT_MAX;
+						nb=0;
+						i= id;
+						break;
+					}
 					i++;
 				}else break;
+			}while(c=read());
+			if(signe == -1) *nAcc = -nb;			
+			else *nAcc= nb;
+			*(nAcc+1) = '\0';
+			fmt++;
+			nb=0;
+			c=' ';
+			signe=0;
+			goto next;
+		case 's':	/* Espace alloué : en cas de dépassement mémoire : impossible de sizeof
+				Cas de nombre de paramètres différents du nombre de format indiqué: impossible de savoir à l'avance le nombre de paramètres */
+			//Pas de protection
+			buf =  va_arg(args, char*); 
+			int j=0;
+			c=' ';
+			while(c=read()){ 
+				if(c>32 && c<127) break;
 			}
-			goto fin;		
-		case 's':
-
-			/* Condition de fin , taille allocation mémoire
-			Probleme de dépassement de mémoire ?*/aprintf("\n\n");
-			//while(read(1,&c,1)){
-			while(c= read()){			
-				if ( c> 32 && c<127){ /* Problème avec les accents*/ //printf("%c",c);
-					*(buf+i) = c;
+              	       do{	if(c!='\0'&& c> 32 && c<127 ) {
+					buf[j]= c;//printf("{(%c) }",c);
 					i++;
+					j++;
 				}
 				else break;
-			}
-			goto fin;
-			
-		default: break;
+			} while(c=read());
+			buf[j]='\0';
+			fmt++;
+			goto next;
+		default: fmt++; goto next;
 	}// fin switch
-
-default : fmt++;
+	}// fin while
+default :  
+	if (*fmt){//printf("\nDEF :%c\n",*fmt); 
+		fmt++;
+		continue;}
+	else goto end;
 } // fin switch
+next: continue; 
+//fmt++;
 } // fin while
-
-fin:
-*(buf+i) = '\0';
-buf = NULL;
+end :
+va_end(args);
 return i;
-}       
+}                               
 
-        
+	
+
+
